9e4ca15323ee72f347cf8d918e6d8aeac03aad2f
URIOXX <danthonyjohan@gmail.com>
2025-06-20 20:44:58 -0400

    Inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './components/homepage';
 
 function App() {
 
   return (
     <>
-      <p>Hello word</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage/>} />
+          <Route path="/about" element={<h1>About Page</h1>} />
+          <Route path="/contact" element={<h1>Contact Page</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/homepage.tsx
@@ -0,0 +1,11 @@
+const Homepage = () => {
+  return (
+    <>
+      <div className="">
+        <h1>Welcome to the Homepage</h1>
+        <p>This is the main content of the homepage.</p>
+      </div>
+    </>
+  );
+};
+export default Homepage;
